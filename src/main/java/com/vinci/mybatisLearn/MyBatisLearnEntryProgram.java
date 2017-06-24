551acc35df7a464451bf17791713996f934a27bd
mt <oc.mt@hotmail.com>
2017-06-24 14:50:34 +0800

    第一次

--- src/main/java/com/vinci/mybatisLearn/MyBatisLearnEntryProgram.java
+++ src/main/java/com/vinci/mybatisLearn/MyBatisLearnEntryProgram.java
@@ -1,7 +1,5 @@
 package com.vinci.mybatisLearn;
 
-import com.vinci.mybatisLearn.annotation.PostgresqlMapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
